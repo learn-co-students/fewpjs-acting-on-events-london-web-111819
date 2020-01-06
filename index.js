623f954dbdde6a324687e1ea33c82072c50af563
badogan <basri.dogan.71@gmail.com>
2020-01-06 14:06:49 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowLeft") {
       moveDodgerLeft();
     }
+  });
+
+  document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowRight") {
         moveDodgerRight();
       }
@@ -19,12 +22,11 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-    let rightNumbers = dodger.style.right.replace("px", "");
-    let right = parseInt(rightNumbers, 10);
-   
-    if (right > 0) {
-      dodger.style.left = `${left + 1}px`;
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`
     }
-}
+  }
 
 
